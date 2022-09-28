a6e074fb74b04454659e79848062d4703eddc94b
伊藤廣軒 <106290558+ItouHiroken@users.noreply.github.com>
2022-09-29 02:49:41 +0900

    [Add]プレイヤーについてるマガジンを回した

---
+++ Assets/Script/Guruguru.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guruguru : MonoBehaviour
+{
+    [SerializeField] float _kaitenSpeed;
+    void Update()
+    {
+        Mawaru();
+    }
+    void Mawaru()
+    {
+        Quaternion rot = Quaternion.AngleAxis(_kaitenSpeed, Vector3.back);// 現在の自信の回転の情報を取得する。
+        Quaternion q = gameObject.transform.rotation;// 合成して、自身に設定
+        gameObject.transform.rotation = q * rot;
+    }
+}
