2daa8984527dc9d743f59a529f5ab9c0efd90e72
伊藤廣軒 <106290558+ItouHiroken@users.noreply.github.com>
2022-10-01 05:04:47 +0900

    [Add]テレポート先をインスタンスした。強化弾も出るようになった

--- Assets/Script/Teleport.cs
+++ Assets/Script/Teleport.cs
@@ -7,8 +7,15 @@ public class Teleport : MonoBehaviour
     [SerializeField] Transform _teleportPoint;
     [SerializeField] List<GameObject> _bullets = new();
     [SerializeField] List<string> _bulletTagName = new();
-    [SerializeField] float _normalBulletPower=10.0f;
-    [SerializeField] float _strongBulletPower=15.0f;
+    [SerializeField] float _normalBulletPower = 10.0f;
+    [SerializeField] float _strongBulletPower = 15.0f;
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(_bulletTagName[2]))//プレイヤー
+        {
+            collision.gameObject.transform.position = _teleportPoint.position;
+        }
+    }
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag(_bulletTagName[0]))//普通
@@ -25,12 +32,17 @@ public class Teleport : MonoBehaviour
         }
         if (collision.CompareTag(_bulletTagName[1]))//強化
         {
-            _bullets[1].gameObject.transform.position = _teleportPoint.position;
+            GameObject a = Instantiate(_bullets[1], _teleportPoint.transform.position, _bullets[1].transform.rotation);
+            Vector3 forceDirection = gameObject.transform.position.normalized - _teleportPoint.transform.position.normalized;
+            Vector3 force = _strongBulletPower * forceDirection;
+            a.layer = collision.gameObject.layer;
+            a.tag = "Untagged";
+            Rigidbody2D rb = a.gameObject.GetComponent<Rigidbody2D>();
+            rb.AddForce(force, ForceMode2D.Impulse);
+            _bullets[0].gameObject.transform.position = _teleportPoint.position;
+            Destroy(collision.gameObject);
         }
-        if (collision.CompareTag(_bulletTagName[2]))//プレイヤー
-        {
-            collision.gameObject.transform.position = _teleportPoint.position;
-        }
+
     }
 
 }
